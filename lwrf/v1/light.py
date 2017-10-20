f1a4c5436fb67862d093c6b629030a591b0e2971
Matthew Macdonald-Wallace <theprofessor@threedrunkensysadsonthe.net>
2017-10-20 16:06:45 +0100

    More tests and better output

--- lwrf/v1/light.py
+++ lwrf/v1/light.py
@@ -54,9 +54,24 @@ class Light():
         sock.sendto(self.message.encode(), ("10.224.231.124", 9760))
 
     def change_state(self, state="OFF", brightness=0):
+        _msg = "State Change Failed"
+        _return_code = 128
         if state == "OFF":
             self._switch_off()
         elif state == "ON":
             self._switch_on()
         elif state == "DIM":
+            if brightness > 32:
+                _msg = """
+Lightwave supports values between 0 (OFF) and 32 (100%) for brightness.
+"""
+                _return_code = 1
+            else:
+                _msg = "Brightness Changed to %s%%" % (brightness / 32 * 100)
+                _return_code = 0
             self._set_brightness(brightness)
+        _ret_obj = {
+            'message': _msg,
+            'return_code': _return_code
+        }
+        return _ret_obj
--- tests/test_v1_lighting.py
+++ tests/test_v1_lighting.py
@@ -7,9 +7,17 @@ class TestV1Lighting(unittest.TestCase):
     def setUp(self):
         self.room = 1
         self.device = 1
-
-    def test_light_device_can_be_created(self):
         self.light = Light(
             room_id=self.room,
             device_id=self.device
         )
+
+    def test_light_brightness_is_not_more_than_32(self):
+        set_state = self.light.change_state(state='DIM',
+                                            brightness=33)
+        self.assertEqual(set_state['return_code'], 1)
+
+    def test_light_brightness_can_be_changed(self):
+        set_state = self.light.change_state(state='DIM',
+                                            brightness=16)
+        self.assertEqual(set_state['return_code'], 0)
